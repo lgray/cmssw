f20b3f318148a28d223cae29f3526cb64f7ed69f
Lindsey Gray <lindsey.gray@gmail.com>
2014-03-31 11:50:15 +0200

    clone PFBlockAlgo for development and improve accessors in PFBlockElement*

--- RecoParticleFlow/PFProducer/plugins/SealModule.cc
+++ RecoParticleFlow/PFProducer/plugins/SealModule.cc
@@ -11,6 +11,7 @@
 #include "RecoParticleFlow/PFProducer/plugins/PFElectronTranslator.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFPhotonTranslator.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFBlockProducer.h"
+#include "RecoParticleFlow/PFProducer/plugins/PFBlockProducerNew.h"
 #include "RecoParticleFlow/PFProducer/plugins/EFilter.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFLinker.h"
 #include "RecoParticleFlow/PFProducer/plugins/PFCandidateFwdPtrProducer.cc"
@@ -23,6 +24,7 @@ DEFINE_FWK_MODULE(PFCandidateChecker);
 DEFINE_FWK_MODULE(PFElectronTranslator);
 DEFINE_FWK_MODULE(PFPhotonTranslator);
 DEFINE_FWK_MODULE(PFBlockProducer);
+DEFINE_FWK_MODULE(PFBlockProducerNew);
 DEFINE_FWK_MODULE(EFilter);
 DEFINE_FWK_MODULE(PFLinker);
 DEFINE_FWK_MODULE(PFCandidateFwdPtrProducer);
