ccf4d60a511405678a3778352a89147b3225b420
Michail <bachtis@cern.ch>
2014-04-14 19:49:49 +0200

    Adding Delta beta weights

--- CommonTools/ParticleFlow/plugins/SealModule.cc
+++ CommonTools/ParticleFlow/plugins/SealModule.cc
@@ -7,16 +7,24 @@
 #include "CommonTools/ParticleFlow/plugins/PFJetFwdPtrProducer.h"
 #include "CommonTools/ParticleFlow/plugins/PFTauFwdPtrProducer.h"
 #include "CommonTools/ParticleFlow/plugins/PFCandidateFromFwdPtrProducer.h"
+//#include "CommonTools/ParticleFlow/plugins/TopProjector.cc"
+#include "CommonTools/ParticleFlow/plugins/DeltaBetaWeights.h"
+
 
 DEFINE_FWK_MODULE(PFMET);
 DEFINE_FWK_MODULE(Type1PFMET);
 DEFINE_FWK_MODULE(PFPileUp);
-
 DEFINE_FWK_MODULE(PFCandidateFwdPtrCollectionStringFilter);
 DEFINE_FWK_MODULE(PFCandidateFwdPtrCollectionPdgIdFilter);
 DEFINE_FWK_MODULE(PFJetFwdPtrProducer);
 DEFINE_FWK_MODULE(PFTauFwdPtrProducer);
 DEFINE_FWK_MODULE(PFCandidateFromFwdPtrProducer);
-
 typedef edm::ProductFromFwdPtrProducer< reco::PFJet >  PFJetFromFwdPtrProducer;
 DEFINE_FWK_MODULE(PFJetFromFwdPtrProducer);
+//DEFINE_FWK_MODULE(TPPFCandidatesOnPFCandidates);
+//DEFINE_FWK_MODULE(TPPFCandidatesOnPileUpPFCandidates);
+//DEFINE_FWK_MODULE(TPIsolatedPFCandidatesOnPFCandidates);
+//DEFINE_FWK_MODULE(TPPFJetsOnPFCandidates);
+//DEFINE_FWK_MODULE(TPPFTausOnPFJets);
+DEFINE_FWK_MODULE(DeltaBetaWeights);
+
