1cf465fd92c4981137a69fa024897ce61c0390b8
Michail <bachtis@cern.ch>
2014-04-14 20:09:13 +0200

    implementing weighting in isolation

--- CommonTools/ParticleFlow/python/deltaBetaWeights_cff.py
+++ CommonTools/ParticleFlow/python/deltaBetaWeights_cff.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from CommonTools.ParticleFlow.deltaBetaWeights_cfi import *
 
-pfDeltaBetaWeighting = cms.Sequence(pfWeightedPhotons,pfWeightedNeutralHadrons)
+pfDeltaBetaWeightingSequence = cms.Sequence(pfWeightedPhotons,pfWeightedNeutralHadrons)
