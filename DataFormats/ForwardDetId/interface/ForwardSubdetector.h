2f31d7e7eafaf2f33547a354dcd5ed6b70e680b1
Andrea Marini <andrea.marini@cern.ch>
2015-09-22 16:11:14 +0200

    reverting the deletion of CFC

--- DataFormats/ForwardDetId/interface/ForwardSubdetector.h
+++ DataFormats/ForwardDetId/interface/ForwardSubdetector.h
@@ -2,6 +2,7 @@
 #define DataFormats_ForwardDetId_ForwardSubDetector_H 1
 
 enum ForwardSubdetector { ForwardEmpty=0, HGCTrigger=1, BHM=2, HGCEE=3, HGCHEF=4, 
-			  HGCHEB=5, HGCHET=6, FastTime=7};
+			  HGCHEB=5, HGCHET=6, FastTime=7, CFC= 999}; 
+	// CFC scheduled to be removed
 
 #endif
