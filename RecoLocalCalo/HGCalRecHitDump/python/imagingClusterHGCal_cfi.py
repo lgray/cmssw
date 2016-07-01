70e1248d46a79d38a83782ee54e3e1a2821ab9d0
Emilio <Emilio.Meschi@cern.ch>
2016-07-01 10:53:38 +0200

    updated Ec for calibrated rechits

--- RecoLocalCalo/HGCalRecHitDump/python/imagingClusterHGCal_cfi.py
+++ RecoLocalCalo/HGCalRecHitDump/python/imagingClusterHGCal_cfi.py
@@ -8,7 +8,7 @@ imagingClusterHGCal =  cms.EDProducer(
     detector = cms.string("EE"),
     doSharing = cms.bool(False),
     deltac = cms.double(2.),
-    ecut = cms.double(0.0001),
+    ecut = cms.double(0.01),
     kappa = cms.double(10.),
     verbosity = cms.untracked.uint32(3)
     )
