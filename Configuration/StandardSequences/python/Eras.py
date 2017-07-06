2e2ed87fec0d65f87acd02b35d8cd7f9e0941b2c
lgray <lindsey.gray@gmail.com>
2017-07-06 15:01:01 -0500

    create miniaod timing reprocessing era

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -29,7 +29,8 @@ class Eras (object):
                  'Phase2C2',
                  'Phase2C1_timing',
                  'Phase2C2_timing',
-                 'Phase2C2_timing_layer']
+                 'Phase2C2_timing_layer',
+                 'Phase2C2_timing_miniaod_reproc']
 
         internalUseMods = ['run2_common', 'run2_25ns_specific',
                            'run2_50ns_specific', 'run2_HI_specific',
@@ -41,7 +42,7 @@ class Eras (object):
                            'phase1Pixel', 'run3_GEM', 'run2_GEM_2017',
                            'phase2_common', 'phase2_tracker',
                            'phase2_hgcal', 'phase2_muon', 'phase2_timing',
-                           'phase2_timing_layer','phase2_hcal',
+                           'phase2_timing_layer','BarrelTDR_timing_reproc_miniaod', 'phase2_hcal',
                            'trackingLowPU', 'trackingPhase1', 'trackingPhase1QuadProp', 'ctpps_2016', 'trackingPhase2PU140',
                            'tracker_apv_vfp30_2016']
         internalUseModChains = ['run2_2017_noTrackingModifier']
