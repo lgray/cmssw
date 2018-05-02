53460ca1d03ff98e310486a71c149decd43fc755
lgray <lindsey.gray@gmail.com>
2018-05-02 16:39:20 -0500

    more geometry record stuff

--- Geometry/Records/interface/PMTDRcd.h
+++
@@ -1,6 +0,0 @@
-#ifndef PMTDRCD_H
-#define PMTDRCD_H
-
-#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
-class PMTDRcd : public edm::eventsetup::EventSetupRecordImplementation<PMTDRcd> {};
-#endif
