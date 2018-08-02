afd62ffb427f7412094935a34610cf4858069a29
lgray <lindsey.gray@gmail.com>
2018-08-02 14:54:03 -0500

    remove first try

--- Geometry/MTDGeometry/src/ES_BTLGeometry.cc
+++
@@ -1,4 +0,0 @@
-#include <Geometry/MTDGeometry/interface/BTLGeometry.h>
-#include <FWCore/Utilities/interface/typelookup.h>
-
-TYPELOOKUP_DATA_REG(BTLGeometry);
--- Geometry/MTDGeometry/src/ES_ETLGeometry.cc
+++
@@ -1,7 +0,0 @@
-#include "Geometry/MTDGeometry/interface/ETLGeometry.h"
-
-#include "FWCore/Utilities/interface/typelookup.h"
-
-TYPELOOKUP_DATA_REG(ETLGeometry);
-
-
